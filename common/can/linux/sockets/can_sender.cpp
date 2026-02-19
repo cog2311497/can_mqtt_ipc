9aec1a222399339a1f2146c9c8230c413c97df87
Maciej Bocianski <maciej.bocianski@cognizant.com>
2026-02-19 09:57:52 +0100

    add licence headers

--- common/can/linux/sockets/can_sender.cpp
+++ common/can/linux/sockets/can_sender.cpp
@@ -1,3 +1,25 @@
+/*
+ * <CAN MQTT IPC>
+ *
+ * Copyright (c) 2026 Cognizant.
+ * All Rights Reserved.
+ *
+ * This software and associated documentation files (the "Software")
+ * are the property of Cognizant.
+ *
+ * Permission is granted to use this Software solely in accordance
+ * with the terms of a valid license agreement with Cognizant.
+ *
+ * Redistribution, modification, sublicensing, or commercial use
+ * of this Software, in whole or in part, is prohibited except as
+ * expressly authorized in writing by Cognizant.
+ *
+ * This Software is provided "AS IS" without warranty of any kind,
+ * express or implied, including but not limited to the warranties
+ * of merchantability, fitness for a particular purpose, and
+ * non-infringement.
+ *
+ */
 
 #include "can/linux/sockets/can_sender.h"
 
